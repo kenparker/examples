9e8cdca0cd1b6d85d78c4f12d5e073b0ef75cab7
angelo <magangde@yahoo.de>
2014-06-11 08:35:05 +0200

    IO Operations added, Read and Write to file using BufferedReaders und Writers

---
+++ src/main/java/com/maggioni/IOOperations/BufferedWriteToFile_1.java
@@ -0,0 +1,65 @@
+package com.maggioni.IOOperations;
+
+import java.io.*;
+
+/**
+ *
+ * @author maggioni
+ */
+public class BufferedWriteToFile_1
+{
+
+  public static void br2bw(BufferedReader r, BufferedWriter w) throws IOException
+  {
+    String z;
+
+    while ((z = r.readLine()) != null)
+    {
+//System.out.println(z);
+      if (!z.equals("quit"))
+      {
+        w.write(z);
+        w.newLine();
+      } else
+      {
+        break;
+      }
+    }
+
+    r.close();
+    w.close();
+  }
+
+  public static void main(String[] args) throws IOException
+  {
+
+    main2();
+  }
+
+  public static void main1() throws IOException
+  {
+    String xFile = "C:/Users/maggioni/Java_Aufgaben/Test_WriterToFile.txt";
+    File fileName = new File(xFile);
+    BufferedReader rb = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter wb = new BufferedWriter(new FileWriter(fileName));
+
+    System.out.println("gib was rein");
+    System.out.println();
+
+    br2bw(rb, wb);
+  }
+  
+  
+  public static void main2() throws IOException
+  {
+    String xFile = "C:/Users/maggioni/Java_Aufgaben/Test_WriterToFile.txt";
+    File fileName = new File(xFile);
+    BufferedReader rb = new BufferedReader(new FileReader(fileName));
+    BufferedWriter wb = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    System.out.println("output");
+    System.out.println();
+
+    br2bw(rb, wb);
+  }
+}
