2db32251dabb3410d44a3341027cd1d27d226b02
angelo <magangde@yahoo.de>
2014-07-16 08:33:59 +0200

    Regular Expressions examples added / JUnit used

---
+++ src/main/java/com/maggioni/regularExpressions/Example_Others/StringMatchExample.java
@@ -0,0 +1,27 @@
+package com.maggioni.regularExpressions.Example_Others;
+
+/**
+ * http://java67.blogspot.de/2012/09/java-string-matches-example-regular-expression.html
+ * @author maggioni
+ */
+public class StringMatchExample {
+
+    public static void main(String args[]) {
+        String[] alphabets = {"", "12345", "A12345", "12345B",
+            "12345a", "abcd", "aa343"};
+
+        for (String alphabet : alphabets) {
+            System.out.println(" does " + alphabet
+                    + " contains alphabetic words : " + alphabet.matches(".*[A-Za-z].*"));
+
+        }
+
+        //checking if String contains digits or not
+        String[] numbers = {"1234", "+1234", "234a"};
+        for (String number : numbers) {
+            System.out.println(" number " + number + " contains only 1-9 digits : "
+                    + number.matches(".*[1-9].*"));
+        }
+
+    }
+}
